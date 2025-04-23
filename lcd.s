2e22c417cc0ebcfb393dc80e3484148a16ba1c9a
Gregory Moyer <greg@commitpush.run>
2025-04-22 21:58:37 -0400

    Check busy flag on LCD

--- lcd.s
+++ lcd.s
@@ -61,7 +61,29 @@ reset:
 loop:
     jmp loop
 
+lcd_wait:
+    pha             ; Save A register
+    lda #%00000000  ; Set Port B pins to input
+    sta IO_DDRB
+
+lcd_wait_busy:
+    lda #LCD_READ
+    sta IO_PORTA
+    lda #(LCD_READ | LCD_ENABLE)
+    sta IO_PORTA
+    lda IO_PORTB    ; Read busy flag
+    and #%10000000  ; Ignore all but the busy flag
+    bne lcd_wait_busy
+
+    lda #0          ; Clear LCD control inputs
+    sta IO_PORTA
+    lda #%11111111  ; Set Port B pins to output
+    sta IO_DDRB
+    pla             ; Restore A register
+    rts
+
 lcd_instruction:
+    jsr lcd_wait
     sta IO_PORTB
     lda #LCD_ENABLE ; Enable LCD
     sta IO_PORTA
@@ -70,6 +92,7 @@ lcd_instruction:
     rts
 
 print_char:
+    jsr lcd_wait
     sta IO_PORTB
     lda #(LCD_ENABLE | LCD_MODE_DATA) ; Enable LCD in data mode
     sta IO_PORTA
