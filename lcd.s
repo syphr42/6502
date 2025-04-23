572f0dee983f86aa5ea0057535ff73dd3b31c999
Gregory Moyer <greg@commitpush.run>
2025-04-22 20:30:36 -0400

    Simplify LCD prog with sub-routines

--- lcd.s
+++ lcd.s
@@ -3,13 +3,16 @@ IO_PORTA = $6001
 IO_DDRB  = $6002
 IO_PORTB = $6000
 
-LCD_ENABLE     = %10000000
-LCD_READ_WRITE = %01000000
-LCD_REG_SELECT = %00100000
+LCD_ENABLE    = %10000000
+LCD_READ      = %01000000
+LCD_MODE_DATA = %00100000
 
     .org $8000
 
 reset:
+    ldx #$ff
+    txs             ; Init stack pointer
+
     lda #%11100000  ; Set data direction (top 3 pins) port A to output
     sta IO_DDRA
     lda #%11111111  ; Set data direction (all pins) port B to output
@@ -18,122 +21,61 @@ reset:
     sta IO_PORTA
 
     lda #%00111000  ; Set 8-bit, 2-line, 5x8 font
-    sta IO_PORTB
-    lda #LCD_ENABLE ; Enable LCD
-    sta IO_PORTA
-    lda #0          ; Clear LCD control inputs
-    sta IO_PORTA
-
+    jsr lcd_instruction
     lda #%00001110  ; Display on, cursor on, blink off
-    sta IO_PORTB
-    lda #LCD_ENABLE ; Enable LCD
-    sta IO_PORTA
-    lda #0          ; Clear LCD control inputs
-    sta IO_PORTA
-
+    jsr lcd_instruction
     lda #%00000110  ; Draw left to right, don't scroll
-    sta IO_PORTB
-    lda #LCD_ENABLE ; Enable LCD
-    sta IO_PORTA
-    lda #0          ; Clear LCD control inputs
-    sta IO_PORTA
-
+    jsr lcd_instruction
     lda #%00000001  ; Clear display
-    sta IO_PORTB
-    lda #LCD_ENABLE ; Enable LCD
-    sta IO_PORTA
-    lda #0          ; Clear LCD control inputs
-    sta IO_PORTA
+    jsr lcd_instruction
 
-    lda #LCD_REG_SELECT ; Select data register
-    sta IO_PORTA
-    lda #"H"            ; Send character
-    sta IO_PORTB
-    lda #(LCD_ENABLE | LCD_REG_SELECT) ; Enable LCD
-    sta IO_PORTA
-    lda #LCD_REG_SELECT ; Disable LCD with data register still selected
-    sta IO_PORTA
-    lda #"e"            ; Send character
-    sta IO_PORTB
-    lda #(LCD_ENABLE | LCD_REG_SELECT) ; Enable LCD
-    sta IO_PORTA
-    lda #LCD_REG_SELECT ; Disable LCD with data register still selected
-    sta IO_PORTA
-    lda #"l"            ; Send character
-    sta IO_PORTB
-    lda #(LCD_ENABLE | LCD_REG_SELECT) ; Enable LCD
-    sta IO_PORTA
-    lda #LCD_REG_SELECT ; Disable LCD with data register still selected
-    sta IO_PORTA
-    lda #"l"            ; Send character
-    sta IO_PORTB
-    lda #(LCD_ENABLE | LCD_REG_SELECT) ; Enable LCD
-    sta IO_PORTA
-    lda #LCD_REG_SELECT ; Disable LCD with data register still selected
-    sta IO_PORTA
-    lda #"o"            ; Send character
-    sta IO_PORTB
-    lda #(LCD_ENABLE | LCD_REG_SELECT) ; Enable LCD
-    sta IO_PORTA
-    lda #LCD_REG_SELECT ; Disable LCD with data register still selected
-    sta IO_PORTA
-    lda #","            ; Send character
-    sta IO_PORTB
-    lda #(LCD_ENABLE | LCD_REG_SELECT) ; Enable LCD
-    sta IO_PORTA
-    lda #LCD_REG_SELECT ; Disable LCD with data register still selected
-    sta IO_PORTA
-    lda #" "            ; Send character
-    sta IO_PORTB
-    lda #(LCD_ENABLE | LCD_REG_SELECT) ; Enable LCD
-    sta IO_PORTA
-    lda #LCD_REG_SELECT ; Disable LCD with data register still selected
-    sta IO_PORTA
-    lda #"L"            ; Send character
-    sta IO_PORTB
-    lda #(LCD_ENABLE | LCD_REG_SELECT) ; Enable LCD
-    sta IO_PORTA
-    lda #LCD_REG_SELECT ; Disable LCD with data register still selected
-    sta IO_PORTA
-    lda #"a"            ; Send character
-    sta IO_PORTB
-    lda #(LCD_ENABLE | LCD_REG_SELECT) ; Enable LCD
-    sta IO_PORTA
-    lda #LCD_REG_SELECT ; Disable LCD with data register still selected
-    sta IO_PORTA
-    lda #"n"            ; Send character
-    sta IO_PORTB
-    lda #(LCD_ENABLE | LCD_REG_SELECT) ; Enable LCD
-    sta IO_PORTA
-    lda #LCD_REG_SELECT ; Disable LCD with data register still selected
-    sta IO_PORTA
-    lda #"d"            ; Send character
-    sta IO_PORTB
-    lda #(LCD_ENABLE | LCD_REG_SELECT) ; Enable LCD
-    sta IO_PORTA
-    lda #LCD_REG_SELECT ; Disable LCD with data register still selected
-    sta IO_PORTA
-    lda #"s"            ; Send character
-    sta IO_PORTB
-    lda #(LCD_ENABLE | LCD_REG_SELECT) ; Enable LCD
-    sta IO_PORTA
-    lda #LCD_REG_SELECT ; Disable LCD with data register still selected
-    sta IO_PORTA
-    lda #"y"            ; Send character
-    sta IO_PORTB
-    lda #(LCD_ENABLE | LCD_REG_SELECT) ; Enable LCD
-    sta IO_PORTA
-    lda #LCD_REG_SELECT ; Disable LCD with data register still selected
-    sta IO_PORTA
-    lda #"!"            ; Send character
-    sta IO_PORTB
-    lda #(LCD_ENABLE | LCD_REG_SELECT) ; Enable LCD
-    sta IO_PORTA
-    lda #LCD_REG_SELECT ; Disable LCD with data register still selected
-    sta IO_PORTA
+    lda #"H"
+    jsr print_char
+    lda #"e"
+    jsr print_char
+    lda #"l"
+    jsr print_char
+    lda #"l"
+    jsr print_char
+    lda #"o"
+    jsr print_char
+    lda #","
+    jsr print_char
+    lda #" "
+    jsr print_char
+    lda #"L"
+    jsr print_char
+    lda #"a"
+    jsr print_char
+    lda #"n"
+    jsr print_char
+    lda #"d"
+    jsr print_char
+    lda #"s"
+    jsr print_char
+    lda #"y"
+    jsr print_char
+    lda #"!"
+    jsr print_char
 
 loop:
     jmp loop
+
+lcd_instruction:
+    sta IO_PORTB
+    lda #LCD_ENABLE ; Enable LCD
+    sta IO_PORTA
+    lda #0          ; Clear LCD control inputs
+    sta IO_PORTA
+    rts
+
+print_char:
+    sta IO_PORTB
+    lda #(LCD_ENABLE | LCD_MODE_DATA) ; Enable LCD in data mode
+    sta IO_PORTA
+    lda #0                            ; Clear LCD control inputs
+    sta IO_PORTA
+    rts
 
     .org $fffc
     .word reset
