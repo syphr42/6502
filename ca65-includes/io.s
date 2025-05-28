f1ca8c63efd474df5daa754a949a40dc97127fe9
Gregory Moyer <greg@commitpush.run>
2025-05-27 22:00:09 -0400

    Add ca65 version of wozmon

---
+++ ca65-includes/io.s
@@ -0,0 +1,53 @@
+; ---
+; Constants for addressing the asynchronous communications interface adapter
+; (ACIA).
+;
+; Address space: 0x5000 - 0x5FFF
+; ---
+
+ACIA_DATA   = $5000     ; 1 byte
+ACIA_STATUS = $5001     ; 1 byte
+ACIA_CMD    = $5002     ; 1 byte
+ACIA_CTRL   = $5003     ; 1 byte
+
+; ---
+; Constants for addressing the versatile interface adapter (VIA).
+;
+; Address space: 0x6000 - 0x6FFF
+; ---
+
+; Register A
+IO_DDRA   = $6003   ; 1 byte
+IO_PORTA  = $6001   ; 1 byte
+
+; Register B
+IO_DDRB   = $6002   ; 1 byte
+IO_PORTB  = $6000   ; 1 byte
+
+; Timer 1
+IO_T1CL   = $6004   ; 1 byte
+IO_T1CH   = $6005   ; 1 byte
+IO_T1LL   = $6006   ; 1 byte
+IO_T1LH   = $6007   ; 1 byte
+
+; Timer 2
+IO_T2CL   = $6008   ; 1 byte
+IO_T2CH   = $6009   ; 1 byte
+
+; Shift Register
+IO_SR     = $600a   ; 1 byte
+
+; Auxilliary Control Register
+IO_ACR    = $600b   ; 1 byte
+
+; Peripheral Control Register
+IO_PCR    = $600c   ; 1 byte
+
+; Interrupt Flag Register
+IO_IFR    = $600d   ; 1 byte
+
+; Interrupt Enable Register
+IO_IER    = $600e   ; 1 byte
+
+; Register A with no handshake
+IO_PORTAN = $600f   ; 1 byte
