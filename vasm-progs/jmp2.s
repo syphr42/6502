a91ad608466a402df1335d43168bede69d123c84
Gregory Moyer <greg@commitpush.run>
2026-08-02 13:21:36 -0400

    Addition jmp testing

--- vasm-progs/jmp2.s
+++ vasm-progs/jmp2.s
@@ -1,32 +1,47 @@
-    .org $0000
     .org $8000
 
 reset:
+    cli
     lda #$40
 
 start:
     asl             ; multiply A by 2
     tax             ; transfer A to X
-    jmp (TABLE,X)   ; $8006 - target is $8007+X
+    jmp (TABLE,X)   ; $8005 - target is $8088+X
 
 TABLE:
-    nop   ; $8007
+    nop   ; $8008
 
-    .org $8087
-    DW  ROUTINE0,ROUTINE1,ROUTINE2   ; $8087
+    .org $8088
+    DW  ROUTINE0,ROUTINE1,ROUTINE2   ; $8088
 
 ROUTINE0:
-    lda #$41        ; $808D
+    lda #$41        ; $808E
     jmp start
 
 ROUTINE1:
-    lda #$42        ; $8092
+    lda #$42        ; $8093
     jmp start
 
 ROUTINE2:
-    lda #$40        ; $8097
+    lda #$40        ; $8098
     jmp start
 
-    .org $fffc
+irq:
+    ldy #$01
+    nop
+    nop
+    nop
+    rti
+
+nmi:
+    ldy #$02
+    nop
+    nop
+    nop
+    rti
+
+    .org $fffa
+    .word nmi
     .word reset
-    .word $0000
+    .word irq
